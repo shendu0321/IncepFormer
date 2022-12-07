4061338d40c95e38b9f5ea9415ae6249ab631044
flh-nuaa <1367353082@qq.com>
2022-12-07 18:07:49 +0800

    modify the voc.py

--- mmseg/datasets/voc.py
+++ mmseg/datasets/voc.py
@@ -29,34 +29,4 @@ class PascalVOCDataset(CustomDataset):
             img_suffix='.jpg', seg_map_suffix='.png', split=split, **kwargs)
         assert osp.exists(self.img_dir) and self.split is not None
 
-@DATASETS.register_module()
-class PascalVOCDataset20(CustomDataset):
-<<<<<<< HEAD
-    """Pascal VOC dataset 20.
-=======
-    """Pascal VOC dataset.
->>>>>>> 332c3634a81dd0339621a0ba2a3ed9158de85465
 
-    Args:
-        split (str): Split txt file for Pascal VOC.
-    """
-
-    CLASSES = ('aeroplane', 'bicycle', 'bird', 'boat', 'bottle',
-               'bus', 'car', 'cat', 'chair', 'cow', 'diningtable', 'dog',
-               'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa',
-               'train', 'tvmonitor')
-
-    PALETTE = [[128, 0, 0], [0, 128, 0], [128, 128, 0], [0, 0, 128],
-               [128, 0, 128], [0, 128, 128], [128, 128, 128], [64, 0, 0],
-               [192, 0, 0], [64, 128, 0], [192, 128, 0], [64, 0, 128],
-               [192, 0, 128], [64, 128, 128], [192, 128, 128], [0, 64, 0],
-               [128, 64, 0], [0, 192, 0], [128, 192, 0], [0, 64, 128]]
-
-    def __init__(self, split, **kwargs):
-        super(PascalVOCDataset20, self).__init__(
-            img_suffix='.jpg', seg_map_suffix='.png', split=split, **kwargs)
-<<<<<<< HEAD
-        assert osp.exists(self.img_dir) and self.split is not None
-=======
-        assert osp.exists(self.img_dir) and self.split is not None
->>>>>>> 332c3634a81dd0339621a0ba2a3ed9158de85465
